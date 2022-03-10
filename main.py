1d2c0298a2e724549faa03f37a3be12535b455ca
Momorider49 <70702025+Momorider49@users.noreply.github.com>
2022-03-10 12:32:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import main
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
+headers = {'User-Agent': ''}
 #L = []
 #nL = []
 
