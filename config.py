58b18bde94c7c7e262037c5f641bd0987f18fbf2
Momorider49 <70702025+Momorider49@users.noreply.github.com>
2022-03-22 11:01:47 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
-#a
+#GitHub Access Token angeben:
+GHAC = ''
+#User Agent hier angeben:
+UserAgent = ''
+#Das Keyword bestimmt wonach gesucht wird
+Keyword = 'bpmn'
+
+#Ein kleines Intervall erhöht die Genauigkeit der Suche
+intervall = 10
+#m Bestimmt den Startpunkt, hier kann die Suche eingeteilt werden
+smallestfilesize = 5000
+#Endpunkt definieren
+largestfilesize = 10000
+#Speicherplatz definieren
+savefolder = "D:/Dokumente/Studium/PD"
+
+#Alle Werte Oberhalb eintragen, dann:
+
+#searchGH ausführen
+#main ausführen
+#SaveTheFiles ausführen
+
+#daraufhin werden alle Dateien in einem Ordner abgespeichert, gemeinsam mit der dazu gehörigen Lizenz Datei
